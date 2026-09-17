d5bf7f919e5b45207622c50855164e0794c771ba
Dave Dhruv <davedhruv0310@gmail.com>
2026-09-17 21:36:15 +0530

    Add files via upload

---
+++ List Comparison and Boolean Logic.py
@@ -0,0 +1,14 @@
+numbers_x = [10, 20, 30, 40, 10]
+
+if numbers_x[0] == numbers_x[-1]:
+    print("Result is True")
+else:
+    print("Result is False")
+
+
+numbers_y = [75,35,43,44,55]
+
+if numbers_y[0] == numbers_y[-1]:
+    print("Result is True")
+else:
+    print("Result is False")
---
+++ Removing Duplicates from a List.py
@@ -0,0 +1,10 @@
+data = [1,2,2,3,4,4,5]
+
+unique = []
+
+for i in data:
+    if i not in unique:
+        unique.append(i)
+
+
+        print("unique elements are:",unique)
